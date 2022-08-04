97538f613f530f34c4e08a416ac44f60fdebe3e8
alberto londono <albertolondono.londono5@gmail.com>
2022-08-04 13:28:25 -0500

    Modify userController with getUserById function to specify a single user

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,5 +6,19 @@ module.exports = {
     User.find({})
       .then((user) => res.json(user))
       .catch((err) => res.status(500).json(err));
-  }
+  },
+
+    //Get single user
+    getUsersById(req, res) {
+        User.findOne({ _id: req.params.userId })
+          .populate("thoughts")
+          .populate("friends")
+          .select("-__v")
+          .then((user) =>
+            !user
+              ? res.status(404).json({ message: "No User find with that ID!" })
+              : res.json(user)
+          )
+          .catch((err) => res.status(500).json(err));
+      }
 }
