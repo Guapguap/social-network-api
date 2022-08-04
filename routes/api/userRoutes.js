8bd70adbd0526f5410bb0a03c68a37082b8492c7
alberto londono <albertolondono.londono5@gmail.com>
2022-08-04 10:06:32 -0500

    Modify userRoutes with methods for specified routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,3 +1,20 @@
 const router = require("express").Router();
 
+// object destructering from userControllers file
+const {
+    getAllUsers,
+    getUsersById,
+    createUsers,
+    updateUsers,
+    deleteUsers
+} = require("../../controllers/userController");
+
+// route for /api/users 
+// get and post methods
+router.route('/').get(getAllUsers).post(createUsers);
+
+// route for /api/users/:id 
+// get put and delete methods
+router.route('/:id').get(getUsersById).put(updateUsers).delete(deleteUsers);
+
 module.exports = router;
