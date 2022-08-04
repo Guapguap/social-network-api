aee87fcd9fe6e64488cf59120b60cb66351c1c9b
alberto londono <albertolondono.londono5@gmail.com>
2022-08-04 12:26:07 -0500

    Create controllers files. Modify thoughtRoutes file with route methods

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,3 +1,35 @@
 const router = require("express").Router();
 
+// object destructering from thoughtController file
+const { 
+    getAllThoughts, 
+    getThoughtsById, 
+    createThoughts, 
+    updateThoughts,
+    deleteThoughts,
+    addReaction,
+    deleteReaction
+
+} = require('../../controllers/thoughts-controller');
+
+// root route
+// get method
+router.route('/').get(getAllThoughts);
+
+// route for /api/thoughts/:id 
+// get, put, and delete methods
+router.route('/:id').get(getThoughtsById).put(updateThoughts).delete(deleteThoughts); 
+
+// route for /api/thoughts/:userId 
+// post method for thought
+router.route('/:userId').post(createThoughts);
+
+// route for /api/thoughts/:thoughtId/reactions 
+// post method for reaction
+router.route('/:thoughtId/reactions').post(addReaction);
+
+// route for /api/thoughts/:thoughtId/reactionId 
+// delete method for specified reaction
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
+
 module.exports = router;
