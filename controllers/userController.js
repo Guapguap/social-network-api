e00b821c18394635c615b43687d19b8cd11d8af7
alberto londono <albertolondono.londono5@gmail.com>
2022-08-04 14:00:57 -0500

    Modify userController.js with updateUsers function to find the specified user by its Id and update the body.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,35 +1,51 @@
 // Requiring the models to create records and to reference those new records
-const {User, Thought} = require('../models');
+const { User, Thought } = require("../models");
 
 module.exports = {
-    //Get all users
-  getAllUsers(req, res) {
+
+//Get all users
+getAllUsers(req, res) {
     User.find({})
-      .then((user) => res.json(user))
-      .catch((err) => res.status(500).json(err));
-  },
+    .then((user) => res.json(user))
+    .catch((err) => res.status(500).json(err));
+},
 
-    //Get single user
-    getUsersById(req, res) {
-        User.findOne({ _id: req.params.userId })
-          .populate("thoughts")
-          .populate("friends")
-          .select("-__v")
-          .then((user) =>
-            !user
-              ? res.status(404).json({ message: "No User find with that ID!" })
-              : res.json(user)
-          )
-          .catch((err) => res.status(500).json(err));
-      },
+//Get single user
+getUsersById(req, res) {
+    User.findOne({ _id: req.params.userId })
+    .populate("thoughts")
+    .populate("friends")
+    .select("-__v")
+    .then((user) =>
+        !user
+        ? res.status(404).json({ message: "No User find with that ID." })
+        : res.json(user)
+    )
+    .catch((err) => res.status(500).json(err));
+},
 
-    //creates a user
-    createUsers(req, res) {
+//creates a user
+createUsers(req, res) {
     User.create(req.body)
-      .then((user) => res.json(user))
-      .catch((err) => {
+    .then((user) => res.json(user))
+    .catch((err) => {
         console.log(err);
         return res.status(500).json(err);
-      });
-  }
-}
+    });
+},
+
+//update a user
+updateUsers(req, res) {
+    User.findOneAndUpdate(
+    { _id: req.params.userId },
+    { $set: req.body },
+    { runValidators: true, new: true }
+    )
+    .then((user) =>
+        !user
+        ? res.status(404).json({ message: "No User find with this ID." })
+        : res.json(user)
+    )
+    .catch((err) => res.status(500).json(err));
+},
+};
