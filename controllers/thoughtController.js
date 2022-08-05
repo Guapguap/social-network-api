589d72b546d77d892bb4371ef89fd9103a4ef09d
alberto londono <albertolondono.londono5@gmail.com>
2022-08-05 14:03:51 -0500

    Modify thoughtController.js with createThoughts function.

---
+++ controllers/thoughtController.js
@@ -0,0 +1,40 @@
+const { User, Thought } = require("../models");
+
+module.exports = {
+    
+// Get all thoughts
+getAllThoughts(req, res) {
+      Thought.find({})
+        .then((thought) => res.json(thought))
+        .catch((err) => res.status(500).json(err));
+    },
+
+// get a specified thought
+getThoughtsById(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+    .select("-__v")
+    .then((thought) =>
+        !thought
+        ? res.status(404).json({ message: "No Thought find with this ID!" })
+        : res.json(thought)
+    )
+    .catch((err) => res.status(500).json(err));
+},
+//create a thought and push the created thought's _id to the associated user's thoughts array field
+createThoughts(req, res) {
+    Thought.create(req.body)
+    .then(({ _id }) => {
+        return User.findOneAndUpdate(
+        { _id: req.body.userId },
+        { $push: { thoughts: _id } },
+        { new: true }
+        );
+    })
+    .then((thought) =>
+        !thought
+        ? res.status(404).json({ message: "No User find with this ID!" })
+        : res.json(thought)
+    )
+    .catch((err) => res.status(500).json(err));
+},
+}
